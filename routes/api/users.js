e516addccd7f99a539e4ce9b0a65585d762ccee4
seanchamanara <seanc@Seans-MacBook-Air.local>
2021-01-04 22:23:46 -0800

    added user validation with express-validator

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,49 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
-// @route   GET api/users
-// @desc    Test route
+const User = require('../../models/user');
+
+// @route   POST api/users
+// @desc    Register user
 // @access  Public
 
 
-router.get('/', (req, res) => res.send('User route'));
+router.post('/', [
+
+  check('name', 'Name is required')
+  .not()
+  .isEmpty(),
+
+  check('email', 'Please enter a valid email')
+  .isEmail(),
+
+  check('password', 'Please enter a password containing 6 or more characters')
+  .isLength({
+    min: 6
+  })
+], 
+async (req, res) => {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() })
+  }
+
+  const { name, email, password } = req.body;
+
+  try {
+      //See if user exists
+      //Get users gravatar
+      // Encrypt password
+      // Return JSON web token
+    
+  } catch (err) {
+    
+  }
+
+
+  res.send('User route');
+
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const app = express();
 
 //Connect Databse
 connectDB();
+
+//Init Middleware
+app.use(express.json({extended: false}));
+
+
 app.get ( '/', (req, res) => res.send('API Running ... ') )
 
 
